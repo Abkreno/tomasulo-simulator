1fbd7f63a7151a22d9aec7a38298cd043260aa6b
Abdelrahman <a.mahmoud1994@gmail.com>
2015-11-21 22:37:39 +0200

    Creating the register package with the class register and registerFile

---
+++ src/Register/Register.java
@@ -0,0 +1,21 @@
+package Register;
+
+public class Register 
+{
+    private short data;
+
+    public Register(short data) 
+    {
+        this.data = data;
+    }
+
+    public short getData() 
+    {
+        return this.data;
+    }
+
+    public void setData(short data) 
+    {
+        this.data = data;
+    }
+}
