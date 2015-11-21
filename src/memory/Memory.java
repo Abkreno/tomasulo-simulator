ec0e6147b5e34fe099b717349271b3b1033d6c72
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-21 18:25:14 +0200

    Adds block size to constructor parameters Adds Method to getBlock from memory

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -1,18 +1,22 @@
 package memory;
 
+import java.util.Arrays;
+
 public class Memory {
 	private short[] memory_data;
-	private int memory_size;
-	private int block_size; // Block Size = # of words inside the block
+	private int memorySize;
+	private int blockSize; // Block Size = # of words inside the block
 
 	public Memory() {
-		this.memory_size = 64 * 1024 / 16;
-		this.memory_data = new short[memory_size];
+		this.memorySize = 64 * 1024 / 16;
+		this.blockSize = memorySize;
+		this.memory_data = new short[memorySize];
 	}
 
-	public Memory(int size) {
-		this.memory_size = size;
-		this.memory_data = new short[memory_size];
+	public Memory(int size, int blockSize) {
+		this.memorySize = size;
+		this.blockSize = blockSize;
+		this.memory_data = new short[memorySize];
 	}
 
 	public void setData(int address, short data) {
@@ -21,5 +25,14 @@ public class Memory {
 
 	public short getData(int address) {
 		return memory_data[address];
+	}
+
+	public int getBlockSize() {
+		return blockSize;
+	}
+
+	public short[] getBlock(int offset) {
+		return Arrays.copyOfRange(memory_data, offset,
+				Math.min(offset + blockSize, memorySize));
 	}
 }
