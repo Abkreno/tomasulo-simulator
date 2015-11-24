b9548a3fe704138592d00b278edb838cdc22f890
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-25 01:25:53 +0200

    Renames packages and variables to follow conventions

--- src/nan/tomasulo/Memory.java
+++ src/nan/tomasulo/Memory.java
@@ -1,9 +1,9 @@
-package memory;
+package nan.tomasulo;
 
 import java.util.Arrays;
 
 public class Memory {
-	private short[] memory_data;
+	private short[] memoryData;
 	private int memorySize;
 	private int blockSize; // Block Size = # of words inside the block
 	private int numOfBlocks;
@@ -12,22 +12,22 @@ public class Memory {
 		this.memorySize = 64 * 1024 / 16;
 		this.blockSize = memorySize;
 		this.numOfBlocks = 1;
-		this.memory_data = new short[memorySize];
+		this.memoryData = new short[memorySize];
 	}
 
 	public Memory(int size, int blockSize) {
 		this.memorySize = size;
 		this.blockSize = blockSize;
 		this.numOfBlocks = size / blockSize + (size % blockSize == 0 ? 0 : 1);
-		this.memory_data = new short[memorySize];
+		this.memoryData = new short[memorySize];
 	}
 
 	public void setData(int address, short data) {
-		memory_data[address] = data;
+		memoryData[address] = data;
 	}
 
 	public short getData(int address) {
-		return memory_data[address];
+		return memoryData[address];
 	}
 
 	public int getBlockSize() {
@@ -41,7 +41,7 @@ public class Memory {
 	 * @return the block that contains the given wordAddress
 	 */
 	public short[] getBlock(int wordAddress) {
-		return Arrays.copyOfRange(memory_data, wordAddress / numOfBlocks,
+		return Arrays.copyOfRange(memoryData, wordAddress / numOfBlocks,
 				Math.min(wordAddress / numOfBlocks + blockSize, memorySize));
 	}
 }
