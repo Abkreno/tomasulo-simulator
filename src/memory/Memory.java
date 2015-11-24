44950f3c0218162e4a140916722af5e4c0aba92c
Abkreno <mohamedelhamamsy789@gmail.com>
2015-11-24 23:48:48 +0200

    modifies getBlock method to return the block in terms of the numOfBlocks

--- src/memory/Memory.java
+++ src/memory/Memory.java
@@ -6,16 +6,19 @@ public class Memory {
 	private short[] memory_data;
 	private int memorySize;
 	private int blockSize; // Block Size = # of words inside the block
+	private int numOfBlocks;
 
 	public Memory() {
 		this.memorySize = 64 * 1024 / 16;
 		this.blockSize = memorySize;
+		this.numOfBlocks = 1;
 		this.memory_data = new short[memorySize];
 	}
 
 	public Memory(int size, int blockSize) {
 		this.memorySize = size;
 		this.blockSize = blockSize;
+		this.numOfBlocks = size / blockSize + (size % blockSize == 0 ? 0 : 1);
 		this.memory_data = new short[memorySize];
 	}
 
@@ -31,8 +34,14 @@ public class Memory {
 		return blockSize;
 	}
 
-	public short[] getBlock(int offset) {
-		return Arrays.copyOfRange(memory_data, offset,
-				Math.min(offset + blockSize, memorySize));
+	/**
+	 * address is the address of the word inside the memory 0 based
+	 * 
+	 * @param address
+	 * @return the block that contains the given wordAddress
+	 */
+	public short[] getBlock(int wordAddress) {
+		return Arrays.copyOfRange(memory_data, wordAddress / numOfBlocks,
+				Math.min(wordAddress / numOfBlocks + blockSize, memorySize));
 	}
 }
