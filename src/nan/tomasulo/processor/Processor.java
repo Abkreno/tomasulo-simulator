f38eefd429512e84bf666d24b4d2a5ea0b70574d
AhmedZaleh <ahmedzaleh7@gmail.com>
2015-11-30 00:46:27 +0200

    Processor initial commit

---
+++ src/nan/tomasulo/processor/Processor.java
@@ -0,0 +1,9 @@
+package nan.tomasulo.processor;
+
+import java.util.LinkedList;
+
+public class Processor {
+	LinkedList<Process> processes;
+	LinkedList<ReservationStation> reservationStations;
+	RegisterResultStatus registerResultStatus;
+}
---
+++ src/nan/tomasulo/processor/RegisterResultStatus.java
@@ -0,0 +1,38 @@
+package nan.tomasulo.processor;
+
+public class RegisterResultStatus {
+	// Each Register holds the current operation
+	
+    public static String F0;
+    public static String F1;
+    public static String F2;
+    public static String F3;
+    public static String F4;
+    public static String F5;
+    public static String F6;
+    public static String F7; 
+    public static String F8;
+    public static String F9;
+    public static String F10;
+    public static String F11;
+    public static String F12;
+    public static String F13;
+    public static String F14;
+    public static String F15;
+    public static String F16;
+    public static String F17; 
+    public static String F18;
+    public static String F19;
+    public static String F20;
+    public static String F21;
+    public static String F22;
+    public static String F23;
+    public static String F24;
+    public static String F25;
+    public static String F26;
+    public static String F27; 
+    public static String F28;
+    public static String F29;
+    public static String F30;
+    public static String F31;
+}
