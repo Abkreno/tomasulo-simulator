10958051140b7b9a77637d1127ff3de2bedd471a
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-02 00:32:08 +0200

    Adds block_size cache_levels to constants

--- src/nan/tomasulo/utils/Constants.java
+++ src/nan/tomasulo/utils/Constants.java
@@ -1,11 +1,15 @@
 package nan.tomasulo.utils;
 
 public class Constants {
+	public static int BLOCK_SIZE;
+	public static int CACHE_LEVELS;
+
 	public enum CacheType {
 		INSTRUCTION_CAHCE, DATA_CACHE
 	}
-	
+
 	public enum WritePolicy {
 		WRITE_THROUGH, WRITE_BACK
 	}
+
 }
