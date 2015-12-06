ba87503909ea88aa3218642288ebdbcd8a253f9b
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-06 22:05:57 +0200

    Modifies register file to array of regs

--- src/nan/tomasulo/registers/RegisterFile.java
+++ src/nan/tomasulo/registers/RegisterFile.java
@@ -1,40 +1,16 @@
 package nan.tomasulo.registers;
 
-public class RegisterFile 
-{
-    //Registers
-    public static Register R0_REGISTER;
-    public static Register R1_REGISTER;
-    public static Register R2_REGISTER;
-    public static Register R3_REGISTER;
-    public static Register R4_REGISTER;
-    public static Register R5_REGISTER;
-    public static Register R6_REGISTER;
-    public static Register R7_REGISTER; 
-    public static Register R8_REGISTER;
-    public static Register R9_REGISTER;
-    public static Register R10_REGISTER;
-    public static Register R11_REGISTER;
-    public static Register R12_REGISTER;
-    public static Register R13_REGISTER;
-    public static Register R14_REGISTER;
-    public static Register R15_REGISTER;
-    public static Register R16_REGISTER;
-    public static Register R17_REGISTER; 
-    public static Register R18_REGISTER;
-    public static Register R19_REGISTER;
-    public static Register R20_REGISTER;
-    public static Register R21_REGISTER;
-    public static Register R22_REGISTER;
-    public static Register R23_REGISTER;
-    public static Register R24_REGISTER;
-    public static Register R25_REGISTER;
-    public static Register R26_REGISTER;
-    public static Register R27_REGISTER; 
-    public static Register R28_REGISTER;
-    public static Register R29_REGISTER;
-    public static Register R30_REGISTER;
-    public static Register R31_REGISTER;
-    
+public class RegisterFile {
+	private static Register[] registers = new Register[32];
+
+	public static void setRegister(int index, short data) {
+		if (index == 0)
+			return;
+		registers[index].setData(data);
+	}
+
+	public static short getRegister(int index) {
+		return registers[index].getData();
+	}
 
 }
