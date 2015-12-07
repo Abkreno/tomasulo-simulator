c3a935406c6979f7354f82c0e17297868ce9956b
Abkreno <mohamedelhamamsy789@gmail.com>
2015-12-07 02:11:22 +0200

    Adds correct/predicted Address field to branch unit

--- src/nan/tomasulo/reservation_stations/BranchUnit.java
+++ src/nan/tomasulo/reservation_stations/BranchUnit.java
@@ -1,8 +1,26 @@
 package nan.tomasulo.reservation_stations;
 
 public class BranchUnit extends ReservationStation {
+	private int predicedAddress, correctAddress;
+
 	public BranchUnit() {
 		super();
+	}
+
+	public int getPredictedAddress() {
+		return predicedAddress;
+	}
+
+	public void setPredicedAddress(int predictionAddress) {
+		this.predicedAddress = predictionAddress;
+	}
+
+	public int getCorrectAddress() {
+		return correctAddress;
+	}
+
+	public void setCorrectAddress(int correctAddress) {
+		this.correctAddress = correctAddress;
 	}
 
 	@Override
